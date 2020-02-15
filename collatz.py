82b3553863106de17dd057badbb27323183f0070
Killian Foley <killianfoley@Killian-Foleys-MacBook.local>
2020-02-15 17:23:59 +0000

    commit

--- collatz.py
+++ collatz.py
@@ -1,3 +1,15 @@
 #This program takes a user input of any positive integer 
 # and outputs the successive values of the following calculation
 
+x = int(input("Please enter a positive integer: "))
+
+print(x)
+
+while x != 1:
+    if x % 2 == 0:
+        x = x // 2
+        print(x)
+
+    elif x % 2 != 0:
+        x = x * 3 + 1
+        print(x)
