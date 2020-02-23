12d5de366e2ccfd4261be47b7076bec43d1de378
Killian Foley <killianfoley@Killian-Foleys-MacBook.local>
2020-02-23 19:03:07 +0000

    Updated reference

--- weekday.py
+++ weekday.py
@@ -5,6 +5,8 @@ import datetime
 
 now = datetime.datetime.now()
 
+#now.weekday returns day or the week as integer between 0 and 6
+#reference https://docs.python.org/2/library/datetime.html
 if now.weekday() >= 0 and now.weekday() <= 4:
     print("Yes, unfortunately today is a weekday.")
 
