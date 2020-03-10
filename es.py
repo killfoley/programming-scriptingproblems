5ba301ad6f20e1de060d74d28a1b3853cc21fc0b
Killian Foley <killianfoley@Killian-Foleys-MacBook.local>
2020-03-10 21:32:42 +0000

    added reference to stack overflow

--- es.py
+++ es.py
@@ -1,6 +1,9 @@
 #This program reads in a text file and returns
 #the number of e's it contains.
 
+#reference to filename import from command line
+#https://stackoverflow.com/questions/7439145/i-want-to-read-in-a-file-from-the-command-line-in-python
+
 import sys
 
 with open(sys.argv[1], 'r') as f:
