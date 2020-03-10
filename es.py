0fe106858581cd1156a337c04ff6ff587823de49
Killian Foley <killianfoley@Killian-Foleys-MacBook.local>
2020-03-10 21:31:33 +0000

    added input from command line argument functionality

--- es.py
+++ es.py
@@ -1,7 +1,9 @@
 #This program reads in a text file and returns
 #the number of e's it contains.
 
-with open("moby-dick.txt", 'r') as f:
+import sys
+
+with open(sys.argv[1], 'r') as f:
     read_data = f.read().replace('\n', '')
     
     c = read_data.count("e")
