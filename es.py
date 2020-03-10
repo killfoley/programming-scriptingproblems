77ffd1e563f494203e6677ba5cfff0ba8b073621
Killian Foley <killianfoley@Killian-Foleys-MacBook.local>
2020-03-10 21:20:22 +0000

    Discovered to count "e" works

--- es.py
+++ es.py
@@ -1,9 +1,9 @@
 #This program reads in a text file and returns
 #the number of e's it contains.
 
-with open("input") as f:
-    read_data = f.read()
+with open("moby-dick.txt", 'r') as f:
+    read_data = f.read().replace('\n', '')
+    
+    c = read_data.count("e")
 
-    c = f.count(e)
-
-    print({} .format(c))
+    print(c)
